e1c97a505fc27b3cab7dde35ea18b34cec230340
alsamancov <alsamancov@gmail.com>
2015-07-07 16:15:23 +0300

    second commit

---
+++ src/students/logic/Group.java
@@ -0,0 +1,47 @@
+package students.logic;
+
+/**
+ * Created by Саманцов on 07.07.2015.
+ */
+public class Group {
+    private int groupId;
+    private String nameGroup;
+    private String curator;
+    private String speciality;
+
+    public String getCurator(){
+        return curator;
+    }
+
+    public void setCurator(String curator){
+        this.curator = curator;
+    }
+
+    public int getGroupId(){
+        return groupId;
+    }
+
+    public void setGroupId(int groupId){
+        this.groupId = groupId;
+    }
+
+    public String getNameGroup(){
+        return nameGroup;
+    }
+
+    public void setNameGroup(String nameGroup){
+        this.nameGroup = nameGroup;
+    }
+
+    public String getSpeciality(){
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality){
+        this.speciality = speciality;
+    }
+
+    public String toString(){
+        return nameGroup;
+    }
+}
